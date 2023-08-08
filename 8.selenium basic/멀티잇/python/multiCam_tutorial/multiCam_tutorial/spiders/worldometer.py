91d8fdb65744f67f05656eddb805388f032ec098
kadeuk <gomsonjangin@gmail.com>
2023-08-08 15:03:31 +0900

    ai

---
+++ 8.selenium basic/멀티잇/python/multiCam_tutorial/multiCam_tutorial/spiders/worldometer.py
@@ -0,0 +1,27 @@
+import scrapy
+
+
+class WorldometerSpider(scrapy.Spider):
+    name = "worldometer"
+    allowed_domains = ["www.worldometers.info"]
+    start_urls = ["https://www.worldometers.info/world-population/population-by-country"]
+
+    def parse(self, response):
+    #     title = response.xpath('//h1/text()').get()
+    #     countries = response.xpath('//td/a/text()').getall()
+    #     yield{
+    #         'title' : title,
+    #         'countries': countries, 
+    #     }
+        countries = response.xpath('//td/a')
+
+        for country in countries:
+            country_name = country.xpath(".//text()").get()
+            link = country.xpath(".//@href").getall()
+
+            yield{
+                'country_name': country_name,
+                'link' : link,
+            }
+
+
